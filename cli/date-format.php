eef5ac670191d8ef6adfe8e35dc43503f2ae80f9
Denis Kravtsov <denis.kravtsov.1986@mail.ru>
2018-08-05 22:45:14 +0300

    rename cli file

--- cli/date-format.php
+++ cli/date-format.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $date_str = $_SERVER['argv'][1] ?? 'now';
 
 try {
-    echo \DenDude\DateHelper::formatDate($date_str);
+    echo \DenDude\DateHelper::formatDate($date_str) . PHP_EOL;
 } catch (Throwable $e) {
     echo $e->getMessage();
     exit(1);
